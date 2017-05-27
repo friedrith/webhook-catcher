a71c31c7cceef05ba4848a919a5cc5ec3ab663ec
Thibault Friedrich <tf@elqui.fr>
2017-05-27 14:32:36 +0200

    display payload

--- src/services/github.js
+++ src/services/github.js
@@ -26,7 +26,7 @@ export default class Github extends Router {
 
     this.router.post('/:appName', (req, res) => {
       const event = req.headers['x-github-event']
-      console.log(req.body.repository)
+      console.log(req.body.payload)
       if (event) {
         if (event === 'push' && req.body.ref && req.body.ref.split('/').length >= 2) {
           const branch = req.body.ref.split('/')[2]
