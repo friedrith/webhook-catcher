25424bb06603eb2859916873fe924c374e5c7abf
Thibault Friedrich <tf@elqui.fr>
2017-05-27 16:07:02 +0200

    correction bug pull request

--- src/services/bitbucket.js
+++ src/services/bitbucket.js
@@ -24,10 +24,10 @@ export default class Bitbucket extends Router {
         if (req.body.push && req.body.push.changes && req.body.push.changes.length > 0 && req.body.push.changes[0].new.type === 'branch') {
           res.sendStatus(204)
           this.emit('push', new PushEvent(req.params.appName, req.body.repository.links.html.href, req.body.push.changes[0].new.name))
-        } else if (req.body.pullrequest && req.body.pullrequest.title && req.body.pullrequest.description && req.body.pullrequest.source && req.body.pullrequest.destination) {
+        } else if (req.body.pullrequest && req.body.pullrequest.title && req.body.pullrequest.source && req.body.pullrequest.destination) {
           res.sendStatus(204)
           console.log('pull request')
-          this.emit('pull-request', new PullRequestEvent(req.params.appName, req.body.repository.links.html.href, req.body.pullrequest.source, req.body.pullrequest.destination, req.body.pullrequest.title, req.body.pullrequest.description))
+          this.emit('pull-request', new PullRequestEvent(req.params.appName, req.body.repository.links.html.href, req.body.pullrequest.source.branch.name, req.body.pullrequest.destination.branch.name, req.body.pullrequest.title, req.body.pullrequest.description))
         } else {
           res.sendStatus(400)
         }
