71fe6283d8db2ef64d90240eac49ffe5e2af7480
Thibault Friedrich <tf@elqui.fr>
2017-05-27 14:30:57 +0200

    change log

--- src/services/github.js
+++ src/services/github.js
@@ -26,7 +26,7 @@ export default class Github extends Router {
 
     this.router.post('/:appName', (req, res) => {
       const event = req.headers['x-github-event']
-      console.log(req.body)
+      console.log(req.body.repository)
       if (event) {
         if (event === 'push' && req.body.ref && req.body.ref.split('/').length >= 2) {
           const branch = req.body.ref.split('/')[2]
